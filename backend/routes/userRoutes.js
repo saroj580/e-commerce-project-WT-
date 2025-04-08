c99d2687f386c12ea91dd1355f21eab1238afa79
saroj580 <chaudharysaroj819@gmail.com>
2025-04-09 00:02:45 +0530

    initialize the routes with userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  updateUserToAdmin,
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.post('/login', authUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser);
+router.route('/:id/make-admin').put(protect, admin, updateUserToAdmin);
+
+export default router; 
